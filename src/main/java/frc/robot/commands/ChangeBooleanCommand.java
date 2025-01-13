557ce000aa3ca828547733c55c74863f15b61b52
glerpstudios <120259946+glerpstudios@users.noreply.github.com>
2025-01-12 19:37:57 -0500

    a

--- src/main/java/frc/robot/commands/ChangeBooleanCommand.java
+++ src/main/java/frc/robot/commands/ChangeBooleanCommand.java
@@ -20,7 +20,7 @@ public class ChangeBooleanCommand extends Command {
   public ChangeBooleanCommand(ChangeBooleanSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(subsystem); 
   }
 
   // Called when the command is initially scheduled.
